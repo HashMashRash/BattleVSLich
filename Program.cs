d9906c52937f84d4603e7c5b2e706d73c8db4848
hashmashrash@mail.ru <hashmashrash@mail.ru>
2019-11-26 12:51:43 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForTestHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Добрый день! Как Вас зовут?  ");
+            string name = Console.ReadLine();
+            Console.Write("Введите, пожалуйста Ваш возраст (полных лет): ");
+            string age = Console.ReadLine();
+            Console.Write("Кто Вы по знаку зодиака?  ");
+            string zodiac = Console.ReadLine();
+            Console.Write("Какова Ваша профессия?  ");
+            string profession = Console.ReadLine();
+            Console.WriteLine($"Вот, что мы знаем о Вас {name}. Возраст: {age}, знак зодиака {zodiac}, Ваша профессия {profession} ");
+            Console.ReadKey();
+        }
+    }
+}
